bfe084506f69429f2f177bb55d8fab2a0d9ee66d
Artem Tyurin <artem.tyurin@gmail.com>
2015-10-15 20:02:19 +0200

    Basic expression support for AST to string transformation

--- ast-stringify/perl-expression.js
+++ ast-stringify/perl-expression.js
@@ -1,3 +1,48 @@
-module.exports = function() {
-	return '[% ' + 'oops' + ' %]';
+var traverse = require('traverse');
+
+function handler() {
+    if (this.node.type === 'Identifier') {
+        return this.update(this.node.name, true);
+    }
+
+    if (this.node.type === 'UnaryExpression') {
+        var operator = this.node.operator;
+
+        return this.update(
+            operator + transform(this.node.argument),
+        true);
+    }
+
+    if (this.node.type === 'Literal') {
+        return this.update(this.node.value, true);
+    }
+
+    if (this.node.type === 'BinaryExpression') {
+        var operator = this.node.operator;
+
+        var left = transform(this.node.left);
+        var right = transform(this.node.right);
+
+        if (operator === 'eq' || operator === 'neq') {
+            right = "'" + right + "'";
+        }
+
+        return this.update(
+            [left, right].join(' ' + operator + ' ')
+        , true);
+    }
+
+    if (this.node.type === 'MemberExpression') {
+        return this.update(
+            transform(this.node.object) + '->{' + transform(this.node.property) + '}'
+        );
+    }
+}
+
+function transform(ast) {
+    return traverse(ast).map(handler);
+}
+
+module.exports = function(node) {
+    return '[% ' + transform(node) + ' %]';
 }
