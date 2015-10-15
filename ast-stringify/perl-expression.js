a65825bd1e5462f7273bd24952fe063f52c665e2
Artem Tyurin <artem.tyurin@gmail.com>
2015-10-15 20:33:39 +0200

    [ast-stringify] Correct order of operations

--- ast-stringify/perl-expression.js
+++ ast-stringify/perl-expression.js
@@ -1,5 +1,12 @@
 var traverse = require('traverse');
 
+var operatorStack = [];
+
+var order = {
+    'and': 1,
+    'or': 2
+};
+
 function handler() {
     if (this.node.type === 'Identifier') {
         return this.update(this.node.name, true);
@@ -19,17 +26,30 @@ function handler() {
 
     if (this.node.type === 'BinaryExpression') {
         var operator = this.node.operator;
+        var previousOperator = operatorStack[operatorStack.length - 1];
+
+        if (operatorStack.length > 0 && order[previousOperator] < order[operator]) {
+            var shouldWrap = true;
+        }
+
+        operatorStack.push(operator);
 
         var left = transform(this.node.left);
         var right = transform(this.node.right);
+
+        operatorStack.pop();
 
         if (operator === 'eq' || operator === 'neq') {
             right = "'" + right + "'";
         }
 
-        return this.update(
-            [left, right].join(' ' + operator + ' ')
-        , true);
+        var out = [left, right].join(' ' + operator + ' ');
+
+        if (shouldWrap) {
+            out = '(' + out + ')';
+        }
+
+        return this.update(out, true);
     }
 
     if (this.node.type === 'MemberExpression') {
