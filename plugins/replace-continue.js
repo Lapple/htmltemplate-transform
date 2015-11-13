b1547a643fe2b24ca7618a08aaab65ccfc9c40a5
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2015-11-13 15:11:35 +0100

    Rework replace-continue to insert TMPL_UNLESS statements on each nested condition

--- plugins/replace-continue.js
+++ plugins/replace-continue.js
@@ -6,7 +6,7 @@ module.exports = function(options) {
 
     assert(Array.isArray(loops), 'Expected options.loopTags to be an array of available loop tags.');
 
-    var continuations = [];
+    var continuations = {};
 
     function isLoop(node) {
         return loops.indexOf(node.name) !== -1;
@@ -20,8 +20,12 @@ module.exports = function(options) {
         if (node.name === 'TMPL_CONTINUE') {
             var parents = this.parents;
 
-            var closestLoopIndex = lastIndexOf(this.parents, function(parent) {
+            var closestLoopIndex = lastIndexOf(parents, function(parent) {
                 return isLoop(parent.node);
+            });
+
+            var conditionParents = parents.slice(closestLoopIndex).filter(function(parent) {
+                return parent.node.type === 'Condition';
             });
 
             var closestContentNodeIndex = lastIndexOf(this.path, function(segment, index, path) {
@@ -37,129 +41,146 @@ module.exports = function(options) {
                     Number(this.path[closestContentNodeIndex])
             );
 
-            var continuation = parents
-                .slice(closestLoopIndex)
-                .reduceRight(function(acc, parent) {
-                    var parentNode = parent.node;
-                    var parentType = parentNode.type;
-                    var rootCondition, precondition;
-
-                    if (parentType === 'ConditionBranch' || parentType === 'AlternateConditionBranch') {
-                        // This code merges preconditions for successive TMPL_IF/
-                        // TMPL_ELSIF checks. For example:
-                        //
-                        //     <TMPL_IF [% $condition_a %]>
-                        //         ## ...
-                        //     <TMPL_ELSIF [% $condition_b %]>
-                        //         <TMPL_CONTINUE>
-                        //     </TMPL_IF>
-                        //
-                        // becomes:
-                        //
-                        //     <TMPL_IF [% !$condition_a && $condition_b %]>
-                        //
-                        if (parentType === 'ConditionBranch') {
-                            rootCondition = parent.parent.parent;
-                            precondition = asExpression(parent.node.condition);
-
-                            for (var i = (index(parent) - 1); i >= 0; i -= 1) {
-                                precondition = binary(
-                                    '&&',
-                                    not(
-                                        asExpression(rootCondition.node.conditions[i].condition)
-                                    ),
-                                    precondition
-                                );
-                            }
-
-                        // This part does the same as above, just adjusted for
-                        // alternate condition, e.g.
-                        //
-                        //     <TMPL_IF [% $condition_a %]>
-                        //         ## ...
-                        //     <TMPL_ELSIF [% $condition_b %]>
-                        //         ## ...
-                        //     <TMPL_ELSE>
-                        //         <TMPL_CONTINUE>
-                        //     </TMPL_IF>
-                        //
-                        // becomes
-                        //
-                        //     <TMPL_UNLESS [% $condition_a || $condition_b %]>
-                        //
-                        } else {
-                            rootCondition = parent.parent;
-                            precondition = not(
-                                rootCondition.node.conditions
-                                    .map(function(branch) {
-                                        return branch.condition;
-                                    })
-                                    .reduce(function(a, b) {
-                                        return binary('||', asExpression(a), asExpression(b));
-                                    })
-                            );
-                        }
-
-                        acc.preconditions.push(precondition);
-                        acc.index = index(rootCondition) + 1;
-
-                        // Any trailing non-conditionally unreachable code should
-                        // be removed.
-                        if (unreachableContentIndex !== -1) {
-                            var contentParent = parents[closestContentNodeIndex];
-
-                            contentParent.update(
-                                contentParent.node.slice(0, unreachableContentIndex)
-                            );
-                        }
-                    }
-
-                    return acc;
-                }, {
-                    index: -1,
-                    preconditions: []
-                });
-
-            if (continuation.preconditions.length === 0) {
+            if (conditionParents.length === 0) {
                 // This part covers case when a wild unconditional `TMPL_CONTINUE`
                 // appears, see `template.008.tmpl` in corresponding test folder.
                 this.parent.update(
                     this.parent.node.slice(0, index(this))
                 );
-            } else {
-                continuations.push(continuation);
             }
+
+            conditionParents.forEach(function(condition) {
+                var key = pathKey(condition.path);
+
+                var continuation = parents
+                    .slice(parents.indexOf(condition))
+                    .reduceRight(function(acc, parent) {
+                        var parentNode = parent.node;
+                        var parentType = parentNode.type;
+                        var rootCondition, precondition;
+
+                        if (parentType === 'ConditionBranch' || parentType === 'AlternateConditionBranch') {
+                            // This code merges preconditions for successive TMPL_IF/
+                            // TMPL_ELSIF checks. For example:
+                            //
+                            //     <TMPL_IF [% $condition_a %]>
+                            //         ## ...
+                            //     <TMPL_ELSIF [% $condition_b %]>
+                            //         <TMPL_CONTINUE>
+                            //     </TMPL_IF>
+                            //
+                            // becomes:
+                            //
+                            //     <TMPL_IF [% !$condition_a && $condition_b %]>
+                            //
+                            if (parentType === 'ConditionBranch') {
+                                rootCondition = parent.parent.parent;
+                                precondition = asExpression(parent.node.condition);
+
+                                for (var i = (index(parent) - 1); i >= 0; i -= 1) {
+                                    precondition = binary(
+                                        '&&',
+                                        not(
+                                            asExpression(rootCondition.node.conditions[i].condition)
+                                        ),
+                                        precondition
+                                    );
+                                }
+
+                            // This part does the same as above, just adjusted for
+                            // alternate condition, e.g.
+                            //
+                            //     <TMPL_IF [% $condition_a %]>
+                            //         ## ...
+                            //     <TMPL_ELSIF [% $condition_b %]>
+                            //         ## ...
+                            //     <TMPL_ELSE>
+                            //         <TMPL_CONTINUE>
+                            //     </TMPL_IF>
+                            //
+                            // becomes
+                            //
+                            //     <TMPL_UNLESS [% $condition_a || $condition_b %]>
+                            //
+                            } else {
+                                rootCondition = parent.parent;
+                                precondition = not(
+                                    rootCondition.node.conditions
+                                        .map(function(branch) {
+                                            return branch.condition;
+                                        })
+                                        .reduce(function(a, b) {
+                                            return binary('||', asExpression(a), asExpression(b));
+                                        })
+                                );
+                            }
+
+                            acc.preconditions.push(precondition);
+                            acc.index = index(rootCondition) + 1;
+
+                            // Any trailing non-conditionally unreachable code should
+                            // be removed.
+                            if (unreachableContentIndex !== -1) {
+                                var contentParent = parents[closestContentNodeIndex];
+
+                                contentParent.update(
+                                    contentParent.node.slice(0, unreachableContentIndex)
+                                );
+                            }
+                        }
+
+                        return acc;
+                    }, {
+                        index: -1,
+                        preconditions: []
+                    });
+
+                if (!Array.isArray(continuations[key])) {
+                    continuations[key] = [];
+                }
+
+                continuations[key].push(continuation);
+            });
         }
 
-        if (isLoop(node)) {
+        if (node.type === 'Condition') {
             this.after(function() {
-                var updated = continuations.reduceRight(function(content, continuation) {
-                    return content
-                        .slice(0, continuation.index)
-                        .concat({
-                            type: 'Condition',
-                            name: 'TMPL_UNLESS',
-                            conditions: [
-                                {
-                                    type: 'ConditionBranch',
-                                    condition: {
-                                        type: 'Expression',
-                                        content: continuation.preconditions.reduceRight(
-                                            binary.bind(null, '&&')
-                                        )
-                                    },
-                                    content: content.slice(continuation.index)
-                                }
-                            ]
-                        });
-                }, node.content);
+                var parent = this.parent;
+                var key = pathKey(this.path);
+                var continuation = continuations[key];
 
-                this.update(
-                    assign({}, node, { content: updated }),
-                    false
-                );
+                if (Array.isArray(continuation)) {
+                    var updated = continuation.reduceRight(function(content, continuation) {
+                        var skippableContent = content.slice(continuation.index);
 
-                continuations = [];
+                        if (skippableContent.length === 0) {
+                            return content;
+                        }
+
+                        return content
+                            .slice(0, continuation.index)
+                            .concat({
+                                type: 'Condition',
+                                name: 'TMPL_UNLESS',
+                                conditions: [
+                                    {
+                                        type: 'ConditionBranch',
+                                        condition: {
+                                            type: 'Expression',
+                                            content: continuation.preconditions.reduceRight(
+                                                binary.bind(null, '&&')
+                                            )
+                                        },
+                                        content: skippableContent
+                                    }
+                                ]
+                            });
+                    }, parent.node);
+
+                    parent.update(updated, false);
+
+                    delete continuations[key];
+                }
             });
         }
     };
@@ -212,4 +233,8 @@ function lastIndexOf(list, fn) {
     }
 
     return -1;
+}
+
+function pathKey(path) {
+    return path.join('.');
 }
